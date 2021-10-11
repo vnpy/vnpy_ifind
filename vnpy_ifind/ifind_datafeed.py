ef0fc657f5687fdd4a3c0b0dcedbbe3b2cb15bf4
vn.py <xiaoyou.chen@foxmail.com>
2021-10-11 09:39:22 +0800

    [Mod] 跑通K线数据下载测试

--- vnpy_ifind/ifind_datafeed.py
+++ vnpy_ifind/ifind_datafeed.py
@@ -1,19 +1,17 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from typing import List, Optional
 from pytz import timezone
 
-from pandas import DataFrame
 from iFinDPy import (
     THS_iFinDLogin,
     THS_HQ,
     THS_HF,
-    THS_SS
+    
 )
 
 from vnpy.trader.setting import SETTINGS
 from vnpy.trader.constant import Exchange, Interval
-from vnpy.trader.object import BarData, TickData, HistoryRequest
-from vnpy.trader.utility import extract_vt_symbol
+from vnpy.trader.object import BarData, HistoryRequest
 from vnpy.trader.datafeed import BaseDatafeed
 
 
@@ -54,104 +52,75 @@ class IfindDatafeed(BaseDatafeed):
         if self.inited:
             return True
 
-        r = THS_iFinDLogin(self.username, self.password)
-        if r:
+        code: int = THS_iFinDLogin(self.username, self.password)
+        if code:
             return False
         return True
 
     def query_bar_history(self, req: HistoryRequest) -> Optional[List[BarData]]:
         """查询K线数据"""
-        if not w.isconnected():
+        if not self.inited:
             self.init()
 
         ifind_exchange = EXCHANGE_MAP[req.exchange]
         ifind_symbol = f"{req.symbol}.{ifind_exchange}"
 
-        indicators = "open;high;low;close;volume"
+        shift = SHIFT_MAP.get(req.interval, None)
+        indicators = "open;high;low;close;volume;amount;openInterest"
 
         if req.interval == Interval.DAILY:
-            error, df = THS_HQ(
-                Codes=ifind_symbol,
-                Indicators=indicators,
-                startdate=req.start.strftime("%Y-%m-%d %H:%M:%S"),
-                enddate=req.end.strftime("%Y-%m-%d %H:%M:%S"),
+            params = "Fill:Original"
+            result = THS_HQ(
+                ifind_symbol,
+                indicators,
+                params,
+                req.start.strftime("%Y-%m-%d %H:%M:%S"),
+                req.end.strftime("%Y-%m-%d %H:%M:%S"),
             )
         else:
-            error, df = THS_HF(
-                Codes=ifind_symbol,
-                Indicators=indicators,
-                startdate=req.start.strftime("%Y-%m-%d %H:%M:%S"),
-                enddate=req.end.strftime("%Y-%m-%d %H:%M:%S"),
+            ifind_interval = INTERVAL_MAP[req.interval]
+            params = f"Fill:Original,Interval:{ifind_interval}"
+
+            result = THS_HF(
+                ifind_symbol,
+                indicators,
+                params,
+                req.start.strftime("%Y-%m-%d %H:%M:%S"),
+                req.end.strftime("%Y-%m-%d %H:%M:%S"),
             )
 
-        if error:
+        if result.errorcode:
             return []
 
         bars: List[BarData] = []
-        for tp in df.itertuples():
+        for tp in result.data.itertuples():
+            if ":" in tp.time:
+                dt = datetime.strptime(tp.time, "%Y-%m-%d %H:%M")
+            else:
+                dt = datetime.strptime(tp.time, "%Y-%m-%d")
+
+            if shift:
+                dt -= shift
+
+            if tp.openInterest:
+                open_interest = tp.openInterest
+            else:
+                open_interest = 0
+
             bar = BarData(
                 symbol=req.symbol,
                 exchange=req.exchange,
-                datetime=tp.Index,
+                datetime=CHINA_TZ.localize(dt),
+                interval=req.interval,
                 open_price=tp.open,
                 high_price=tp.high,
                 low_price=tp.low,
                 close_price=tp.close,
-                # volume=tp.volume,
-                # turnover=tp.turnover,
-                # open_interest=tp.oi,
+                volume=tp.volume,
+                turnover=tp.amount,
+                open_interest=open_interest,
                 gateway_name="IFIND"
             )
             bars.append(bar)
 
         return bars
-
-    def query_tick_history(self, req: HistoryRequest) -> Optional[List[TickData]]:
-        """查询Tick数据"""
-        if not w.isconnected():
-            self.init()
-
-        ifind_exchange = EXCHANGE_MAP[req.exchange]
-        ifind_symbol = f"{req.symbol}.{ifind_exchange}"
-
-        indicators = (
-            "open;high;low;last;volume;turnover;oi;"
-            "bid1;bid2;bid3;bid4;bid5;"
-            "ask1;ask2;ask3;ask4;ask5;"
-        )
-
-        df: DataFrame = THS_SS(
-            codes=ifind_symbol,
-            Indicators=indicators,
-            startdate=req.start.strftime("%Y-%m-%d %H:%M:%S"),
-            enddate=req.end.strftime("%Y-%m-%d %H:%M:%S"),
-        )
-
-        ticks: List[TickData] = []
-        for tp in df.itertuples():
-            tick = TickData(
-                symbol=req.symbol,
-                exchange=req.exchange,
-                datetime=tp.Index,
-                open_price=tp.open,
-                high_price=tp.high,
-                low_price=tp.low,
-                last_price=tp.last,
-                volume=tp.volume,
-                turnover=tp.turnover,
-                open_interest=tp.oi,
-                bid_price_1=tp.bid1,
-                bid_price_2=tp.bid2,
-                bid_price_3=tp.bid3,
-                bid_price_4=tp.bid4,
-                bid_price_5=tp.bid5,
-                ask_price_1=tp.ask1,
-                ask_price_2=tp.ask2,
-                ask_price_3=tp.ask3,
-                ask_price_4=tp.ask4,
-                ask_price_5=tp.ask5,
-                gateway_name="IFIND"
-            )
-            ticks.append(tick)
-
-        return ticks
